4b9f17a98a702da42aaad779e76286d8c93c5286
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-09-21 10:19:00 -0600

    fix error

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ var every = require('schedule').every;
 var connections = [];
 
 var chat = sockjs.createServer();
-var id1percentComplete = 0;
-var id2percentComplete = 0;
-var id3percentComplete = 0;
+
 
 chat.on('connection', function(conn) {
+  var id1percentComplete = 0;
+  var id2percentComplete = 0;
+  var id3percentComplete = 0;
+  var error = '"darkseagreen"';
     //connections.push(conn);
     // var number = connections.length;
     // conn.write("Welcome, User " + number);
+    conn.write('{"NodeId": "1", "percentComplete": 0, "parent": "null", "status":"skyblue", "children": [{"NodeId": "2", "percentComplete": 0, "parent": "Top Level", "status": "skyblue", "children": [{ "NodeId": "4", "percentComplete": '+ id2percentComplete +', "parent": "Level 2: A", "status": "darkseagreen"}, { "NodeId": "5", "percentComplete": '+ id1percentComplete + ', "parent": "Level 2: A", "status": "darkseagreen"}]}, { "NodeId": "3", "percentComplete": ' + id1percentComplete + ', "parent": "Top Level", "value": 10, "status": '+ error + '}]}');
     every('3s').do(function() {
-        id1percentComplete = Math.round(id1percentComplete + 2.5);
+        id1percentComplete = Math.round(id1percentComplete + 2.7);
         id2percentComplete = Math.round(id2percentComplete + 1.3);
         id3percentComplete += 2;
-        conn.write('{"NodeId": "1", "percentComplete": 0, "parent": "null", "status":"skyblue", "children": [{"NodeId": "2", "percentComplete": 0, "parent": "Top Level", "status": "skyblue", "children": [{ "NodeId": "4", "percentComplete": '+ id2percentComplete +', "parent": "Level 2: A", "status": "darkseagreen"}, { "NodeId": "5", "percentComplete": '+ id1percentComplete + ', "parent": "Level 2: A", "status": "darkseagreen"}]}, { "NodeId": "3", "percentComplete": ' + id1percentComplete + ', "parent": "Top Level", "value": 10, "status": "crimson"}]}');
+        if (id1percentComplete > 15) {
+          error ='"crimson"';
+          id1percentComplete = 15;
+        }
+        conn.write('{"NodeId": "1", "percentComplete": 0, "parent": "null", "status":"skyblue", "children": [{"NodeId": "2", "percentComplete": 0, "parent": "Top Level", "status": "skyblue", "children": [{ "NodeId": "4", "percentComplete": '+ id2percentComplete +', "parent": "Level 2: A", "status": "darkseagreen"}, { "NodeId": "5", "percentComplete": '+ id3percentComplete + ', "parent": "Level 2: A", "status": "darkseagreen"}]}, { "NodeId": "3", "percentComplete": ' + id1percentComplete + ', "parent": "Top Level", "value": 10, "status": '+ error + '}]}');
         connections.push(conn);
     });
 
@@ -31,13 +38,13 @@ chat.on('connection', function(conn) {
 
     });
     conn.on('close', function() {
-        for (var ii = 0; ii < connections.length; ii++) {
-            //connections[ii].write("User " + number + " has disconnected");
-            connections[ii].write("disconnected");
-        }
-        var id1percentComplete = 0;
-        var id2percentComplete = 0;
-        var id3percentComplete = 0;
+        // for (var ii = 0; ii < connections.length; ii++) {
+        //     //connections[ii].write("User " + number + " has disconnected");
+        //     connections[ii].write("disconnected");
+        // }
+        // var id1percentComplete = 0;
+        // var id2percentComplete = 0;
+        // var id3percentComplete = 0;
     });
 });
 
